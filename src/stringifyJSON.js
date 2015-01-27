fa1f4b8a352b7d06db453b0f5b53a8539882bf56
Sean Kim <skim2636@gmail.com>
2015-01-26 18:43:00 -0800

    finished stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,63 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var tempArr = [];
+
+
+  if (obj === null) {
+    console.log('Condition 1 is ' + (obj === null));
+    return 'null';
+  }
+
+  if (typeof obj === 'boolean') {
+    console.log('Condition 2 is ' + (typeof obj === 'boolean'));
+    return String(obj);
+  }
+
+  if (typeof obj === 'string') {
+    console.log('Condition 3 is ' + (typeof obj === 'string'));
+    return '"' + obj + '"';
+  }
+
+  if (typeof obj === 'number') {
+    console.log("Condition 4 is " + (typeof obj === 'number'));
+    console.log(typeof obj + ' is a number');
+    return String(obj);
+  }
+
+  if (typeof obj === 'function' || typeof obj === 'undefined') {
+    console.log("Condition 5 is " + (typeof obj === 'function' || typeof obj === 'undefined'));
+    console.log(obj + ' is a function or undefined');
+    return;
+  }
+
+  if (Array.isArray(obj)) {
+
+    if (obj.length === 0) {
+      return '[]';
+    }
+
+    for (var i = 0; i < obj.length; i++) {
+      tempArr[i] = stringifyJSON(obj[i]);
+      console.log('[' + tempArr.join(',') + ']');
+    }
+
+    return String('[' + tempArr.join(',') + ']');
+  } else {
+
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined'){
+        return '{}';
+      }
+
+      if (obj.hasOwnProperty(key)) {
+        tempArr.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+        console.log(String("'{" + tempArr + "}'"));
+      }
+    }
+    return String("{" + tempArr + "}");
+  }
+
 };
