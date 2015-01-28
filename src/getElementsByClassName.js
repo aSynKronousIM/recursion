824458f63d1e9910ae7f164cdd286a62a8022388
Sean Kim <skim2636@gmail.com>
2015-01-27 22:22:40 -0800

    getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,40 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+
+var getElementsByClassName = function(className) {
+  var docList = document.all;
+  var resArray = [];
+
+  console.log(docList);
+
+  var re1 = new RegExp("(?:^|\\s)" + className + "(?:\\s|$)");
+  for (var i = 0; i < docList.length; i++) {
+
+    if (re1.test(docList[i].className))  {
+      resArray[resArray.length] = docList[i];
+    }
+
+  //  if (docList[i].className === className)  {
+  //    console.log("The element at index " + i + " is " + docList[i].className);
+  //    console.log(String(docList[i].className));
+  //    resArray[resArray.length] = docList[i];
+  //  } else if (docList[i].className.length > className.length) {
+  //    docList = docList[i].className.split(' ');
+  //    console.log("docList2 is " + docList + " with length of " + docList.length);
+  //    console.log("The element at index " + i + " is " + docList[i].className);
+  //
+  //    for (var j = 0; j < docList.length; j++) {
+  //      if (docList[j] === className) {
+  //        console.log(docList[j]);
+  //        resArray[resArray.length] = docList[i];
+  //      }
+  //    }
+  //  } else {
+  //    console.log("The element at index " + i + " is " + docList[i].className);
+  //  }
+  //
+  }
+
+  return resArray;
 };
